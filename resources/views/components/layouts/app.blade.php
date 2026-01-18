d804e53c76263be00f43053eca7a1dc40b89596a
owenbcoding <eoghankb@gmail.com>
2026-01-18 12:40:17 +0000

    fixed meta data viewport

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game-App</title>
     <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
