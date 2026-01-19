313519de78394993963ac71285fca8337a30217f
owenbcoding <eoghankb@gmail.com>
2026-01-19 18:02:59 +0000

    refactored popular games section with game-card component

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -1,29 +1,7 @@
    <div wire:init="loadPopularGames"
        class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
        @forelse ($popularGames as $game)
-           <div class="game mt-8">
-               <div class="relative inline-block">
-                   <a href="{{ route('games.show', $game['slug'] ?? '') }}">
-                       <img src="{{ $game['coverImageUrl'] }}" alt="game-cover"
-                           class="hover:opacity-75 transition ease-in-out duration-150 w-full">
-                   </a>
-
-                   <div class="absolute bottom-2 right-2 w-16 h-16 bg-gray-800 text-white rounded-full flex items-center justify-center"
-                       style="right: -29px; bottom: -29px;">
-                       <div class="font-semibold text-xs flex justify-center items-center h-full">
-                           {{ $game['rating'] }}
-                       </div>
-                   </div>
-
-               </div>
-               <a href="{{ route('games.show', $game['slug'] ?? '') }}"
-                   class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">
-                   {{ $game['name'] ?? 'Unknown Game' }}
-               </a>
-               <div class="text-gray-400 mt-1">
-                   {{ $game['platforms'] }}
-               </div>
-           </div>
+           <x-game-card :game="$game" />
        @empty
            @foreach (range(1, 12) as $game)
                <div class="game animate-pulse mt-8">
