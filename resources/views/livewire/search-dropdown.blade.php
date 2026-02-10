27fd69f83718e2774cb8db5cd03c90859a3bd36c
owenbcoding <eoghankb@gmail.com>
2026-02-10 19:58:42 +0000

    created search dropdown files

---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,10 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full pl-10 pr-3 py-1 w-64" placeholder="Search . . .">
+                    <!-- Search Icon -->
+                    <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M16 10a6 6 0 1 1-12 0 6 6 0 0 1 12 0z" />
+                    </svg>
+                </div>
+</div>
