08aaae63ac897f9ae0729cdbd681b1e55127c006
owenbcoding <eoghankb@gmail.com>
2026-02-10 19:58:14 +0000

    refactored hard coded search bar with livewire component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -27,13 +27,8 @@
         
             <!-- Right Section: Search & Avatar -->
             <div class="flex items-center space-x-4 mt-6 lg:mt-0">
-                <div class="relative">
-                    <input type="text" class="bg-gray-800 text-sm rounded-full pl-10 pr-3 py-1 w-64" placeholder="Search . . .">
-                    <!-- Search Icon -->
-                    <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M16 10a6 6 0 1 1-12 0 6 6 0 0 1 12 0z" />
-                    </svg>
-                </div>
+                <livewire:search-dropdown />
+                
                 <a href="#">
                     <img class="w-10 h-10 rounded-full" src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" alt="avatar">
                 </a>
