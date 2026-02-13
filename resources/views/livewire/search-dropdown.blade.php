5ab7740166eb4c3e85296774096977c9b1b27b50
owenbcoding <eoghankb@gmail.com>
2026-02-13 21:21:13 +0000

    created animated spin for search bar

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -9,9 +9,14 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M16 10a6 6 0 1 1-12 0 6 6 0 0 1 12 0z" />
             </svg>
         </div>
-
+        <div wire:loading wire:target="search" class="absolute top-0 right-0 mr-4 mt-2">
+            <svg class="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
         @if (strlen($search) >= 2)
-            <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+            <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-1">
                 <ul>
                     @forelse ($searchResults as $game)
                         <li class="border-b border-gray-700 last:border-b-0">
